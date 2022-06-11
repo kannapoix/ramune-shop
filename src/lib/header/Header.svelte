3331725cdada27290a9e0c9418a22dbdd67325bd
kanna <kanna@protonmail.ch>
2022-06-11 17:48:21 +0900

    Initialize SvelteKit

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import logo from './svelte-logo.svg';
+</script>
+
+<header>
+	<div class="corner">
+		<!-- <a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a> -->
+	</div>
+
+	<h1>オフィス ラムネ</h1>
+
+	<div class="corner">
+		<!-- TODO put something else here? github link? -->
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+
+	h1 {
+		color: white;
+	}
+</style>
---
+++ src/routes/items.svelte
@@ -0,0 +1,76 @@
+<script context="module">
+	export const prerender = true;
+
+	const ramunePrice = import.meta.env.VITE_RAMUNE_PRICE;
+	const shopPath = import.meta.env.VITE_SHOP_PATH;
+	const actionEndpoint = import.meta.env.VITE_ACTION_ENDPOINT;
+</script>
+
+<svelte:head>
+	<title>Store items</title>
+	<meta name="description" content="item list" />
+</svelte:head>
+
+<div class="wrapper">
+	<div class="cell">
+		<picture>
+			<img src="sweets_ramune_drink.png" alt="ramune drink paint" />
+		</picture>
+
+		<p class="item-name">森永大粒ラムネ</p>
+		<p class="price">¥{ramunePrice}</p>
+		<form method="POST" action={actionEndpoint}>
+			<input type="hidden" name="orderId" value="CustomOrderId" />
+			<input type="hidden" name="redirectUrl" value={shopPath} />
+			<button type="submit" name="choiceKey" value={'ramune'}>いますぐ購入</button>
+		</form>
+	</div>
+	<div class="cell">
+		<picture>
+			<img src="sweets_ramune_drink.png" alt="ramune drink paint" />
+		</picture>
+
+		<p class="item-name">森永大粒ラムネ ミックスベリーソーダ味</p>
+		<p class="price">¥{ramunePrice}</p>
+
+		<form method="POST" action={actionEndpoint}>
+			<input type="hidden" name="orderId" value="CustomOrderId" />
+			<input type="hidden" name="redirectUrl" value={shopPath} />
+			<button type="submit" name="choiceKey" value={'ramune'}>いますぐ購入</button>
+		</form>
+	</div>
+</div>
+
+<style>
+	.item-name {
+		font-size: 1.5rem;
+		margin-top: 2rem;
+	}
+	.price {
+		font-size: 3rem;
+		margin-bottom: 0;
+	}
+
+	.cell {
+		text-align: center;
+	}
+
+	img {
+		height: 200px;
+		width: 200px;
+		margin-top: 5rem;
+	}
+
+	.wrapper {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-auto-columns: minmax(200px, auto);
+		grid-auto-rows: minmax(200px, auto);
+	}
+
+	@media (max-width: 480px) {
+		.wrapper {
+			grid-template-columns: repeat(1, 1fr);
+		}
+	}
+</style>
