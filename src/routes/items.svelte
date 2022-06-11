708815e78b988154e39e16d1f85a01734299a302
kanna <kanna@protonmail.ch>
2022-06-11 18:03:42 +0900

    POC BTCPay and deploy to netlify

---
+++ src/routes/items.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Items</title>
+	<meta name="description" content="Item list" />
+</svelte:head>
+
+<picture>
+	<img src="sweets_ramune_drink.png" alt="Welcome" />
+</picture>
+
+<form method="POST" action="https://btcpay.kannadot.tokyo/apps/24Z3sbBgGGpmb1NukZjxJj2vgnxY/pos">
+	<input type="hidden" name="orderId" value="CustomOrderId" />
+	<button type="submit" name="choiceKey" value={'ramune'}>ほしい</button>
+</form>
